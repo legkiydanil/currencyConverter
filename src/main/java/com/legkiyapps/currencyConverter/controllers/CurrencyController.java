9928ce01c452d6e011c5f4ee6798f9ee37e3a080
legkiydanil <legkiy.danil@gmail.com>
2024-10-06 18:09:36 +0300

    Initial commit

--- src/main/java/com/legkiyapps/currencyConverter/controllers/CurrencyController.java
+++ src/main/java/com/legkiyapps/currencyConverter/controllers/CurrencyController.java
@@ -1,11 +1,12 @@
-package com.legkiyapps.currencyConverter;
+package com.legkiyapps.currencyConverter.controllers;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Controller {
+public class CurrencyController {
+
 
     @Value("${spring.application.name}")
     private String name;
